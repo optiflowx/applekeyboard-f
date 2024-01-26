da399ea4d084382d0f4e4e1d844fe6c4881d772a
OptiFlowX <103578263+pabbiebottoman@users.noreply.github.com>
2024-01-26 04:01:21 +0200

    --Code Refactor

--- app/src/main/java/com/optiflowx/applekeyboard/views/global/KeyboardView.kt
+++ app/src/main/java/com/optiflowx/applekeyboard/views/global/KeyboardView.kt
@@ -1,26 +1,20 @@
 package com.optiflowx.applekeyboard.views.global
 
 import android.view.inputmethod.EditorInfo
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.mandatorySystemGesturesPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import com.optiflowx.applekeyboard.services.IMEService
-import com.optiflowx.applekeyboard.utils.KeyboardType
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.optiflowx.applekeyboard.core.enums.KeyboardType
+import com.optiflowx.applekeyboard.core.preferences.PreferencesConstants
+import com.optiflowx.applekeyboard.core.services.IMEService
 import com.optiflowx.applekeyboard.viewmodels.KeyboardViewModel
+import com.optiflowx.applekeyboard.views.clipboard.ClipboardKeyboardView
 import com.optiflowx.applekeyboard.views.emoji.EmojiKeyboardView
 import com.optiflowx.applekeyboard.views.normal.NormalKeyboardView
 import com.optiflowx.applekeyboard.views.number.NumberKeyboardView
@@ -31,25 +25,45 @@ import kotlinx.coroutines.yield
 import splitties.experimental.ExperimentalSplittiesApi
 import splitties.views.InputType
 
-@OptIn(ExperimentalSplittiesApi::class)
+//Should be stable!
+
 @Composable
+@OptIn(ExperimentalSplittiesApi::class)
 fun KeyboardView(viewModel: KeyboardViewModel) {
     val context = LocalContext.current
+
     val keyboardType = viewModel.keyboardType.observeAsState()
+
+    val showTopView = (keyboardType.value != KeyboardType.Symbol)
+
+    val showBottomView = (keyboardType.value != KeyboardType.Number
+            && keyboardType.value != KeyboardType.Phone)
+
+    val locale = viewModel.preferences.getFlowPreference(
+        PreferencesConstants.LOCALE_KEY, "English"
+    ).collectAsStateWithLifecycle("English").value
+
+    val fontType = viewModel.preferences.getFlowPreference(
+        PreferencesConstants.FONT_TYPE_KEY, "Regular"
+    ).collectAsStateWithLifecycle("Regular").value
 
     LaunchedEffect(Unit) {
         while (true) {
             val editor = (context as IMEService).currentInputEditorInfo
+
             when (editor.inputType and EditorInfo.IME_MASK_ACTION) {
-                InputType.number.value ->
+                InputType.number.value -> {
                     viewModel.keyboardType.value = KeyboardType.Number
+                }
 
-                InputType.phone.value ->
+                InputType.phone.value -> {
                     viewModel.keyboardType.value = KeyboardType.Phone
+                }
 
-                else -> if (keyboardType.value != KeyboardType.Emoji) {
-                    if (keyboardType.value != KeyboardType.Symbol) {
-                        viewModel.keyboardType.value = KeyboardType.Normal
+                else -> {
+                    viewModel.keyboardType.value = when (keyboardType.value) {
+                        KeyboardType.Emoji, KeyboardType.Symbol, KeyboardType.Clipboard -> keyboardType.value
+                        else -> KeyboardType.Normal
                     }
                 }
             }
@@ -58,42 +72,34 @@ fun KeyboardView(viewModel: KeyboardViewModel) {
         }
     }
 
-
     Box(
-        Modifier.mandatorySystemGesturesPadding()
+        Modifier
+            .mandatorySystemGesturesPadding()
     ) {
         Column {
-            if (keyboardType.value != KeyboardType.Symbol && keyboardType.value != KeyboardType.Emoji) {
-                KeyboardTopView(viewModel)
+            if (showTopView) KeyboardTopView(viewModel, locale, keyboardType)
+
+//            AnimatedContent(
+//                keyboardType.value,
+//                label = "KeyboardView",
+//                transitionSpec = { (fadeIn()).togetherWith(fadeOut()) },
+//            ) { type ->
+            when (keyboardType.value!!) {
+                KeyboardType.Normal -> NormalKeyboardView(viewModel)
+
+                KeyboardType.Symbol -> SymbolAKeyboardView(viewModel)
+
+                KeyboardType.Number -> NumberKeyboardView(viewModel)
+
+                KeyboardType.Phone -> PhoneNumberKeyboardView(viewModel)
+
+                KeyboardType.Emoji -> EmojiKeyboardView(viewModel)
+
+                KeyboardType.Clipboard -> ClipboardKeyboardView(viewModel)
             }
+//            }
 
-            AnimatedContent(
-                keyboardType,
-                label = "KeyboardView",
-                transitionSpec = {
-                    (fadeIn(animationSpec = tween(250, delayMillis = 5)) +
-                            scaleIn(initialScale = 0.9f, animationSpec = tween(250, delayMillis = 5)))
-                        .togetherWith(fadeOut(animationSpec = tween(5)))
-                },
-            ) { type ->
-                when (type.value!!) {
-                    KeyboardType.Normal -> NormalKeyboardView(viewModel)
-
-                    KeyboardType.Symbol -> SymbolAKeyboardView(viewModel)
-
-                    KeyboardType.Number -> NumberKeyboardView(viewModel)
-
-                    KeyboardType.Phone -> PhoneNumberKeyboardView(viewModel)
-
-                    KeyboardType.Emoji -> EmojiKeyboardView(viewModel)
-                }
-            }
-            //Global Spacer
-            Spacer(modifier = Modifier.height(18.dp))
-
-            if (keyboardType.value != KeyboardType.Number && keyboardType.value != KeyboardType.Phone) {
-                KeyboardBottomView(viewModel)
-            }
+            if (showBottomView) KeyboardBottomView(viewModel, locale, fontType)
         }
     }
 }
