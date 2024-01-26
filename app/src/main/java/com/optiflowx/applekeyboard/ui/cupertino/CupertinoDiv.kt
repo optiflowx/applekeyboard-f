de07365ead68068f2fc1461c7705f49b419f661e
OptiFlowX <103578263+pabbiebottoman@users.noreply.github.com>
2024-01-26 04:37:32 +0200

    --Cupertino Widgets Impl

---
+++ app/src/main/java/com/optiflowx/applekeyboard/ui/cupertino/CupertinoDiv.kt
@@ -0,0 +1,17 @@
+package com.optiflowx.applekeyboard.ui.cupertino
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.alexzhirkevich.cupertino.CupertinoDivider
+import io.github.alexzhirkevich.cupertino.theme.CupertinoColors
+import io.github.alexzhirkevich.cupertino.theme.systemGray7
+
+@Composable
+fun CupertinoDiv() {
+    return CupertinoDivider(
+        modifier = Modifier,
+        thickness = 1.dp,
+        color = CupertinoColors.systemGray7,
+    )
+}
